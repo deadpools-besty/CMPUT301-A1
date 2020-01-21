0a48aad921ad943237e8756e312e4ab900c3e161
Nana Andoh <nanaegyir2000@hotmail.com>
2020-01-20 22:04:24 -0700

    Added card.xml and views required for it

--- app/src/main/java/com/example/android/cardiobook/DiastolicBP.java
+++ app/src/main/java/com/example/android/cardiobook/DiastolicBP.java
@@ -9,4 +9,6 @@ public class DiastolicBP extends BloodPressure {
         }
         else return false;
     }
+
+
 }
--- app/src/main/java/com/example/android/cardiobook/MainActivity.java
+++ app/src/main/java/com/example/android/cardiobook/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.android.cardiobook;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Measurement> measurements = new ArrayList<>();
+
     }
 
 
