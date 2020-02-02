795a49de739c252571de32483cdb46cdfb76b934
Nana Andoh <nanaegyir2000@hotmail.com>
2020-02-01 18:37:36 -0700

    Fixed comment always showing as null

--- app/src/main/java/com/example/android/cardiobook/AddMeasurementDialog.java
+++ app/src/main/java/com/example/android/cardiobook/AddMeasurementDialog.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,16 +64,22 @@ public class AddMeasurementDialog extends DialogFragment {
         sBP = view.findViewById(R.id.systolicBPEditText);
         comment = view.findViewById(R.id.commentEditText);
 
+        // Calendar stuff
+        Calendar cal = Calendar.getInstance();
+        final int year = cal.get(Calendar.YEAR);
+        final int month = cal.get(Calendar.MONTH);
+        final int day = cal.get(Calendar.DAY_OF_MONTH);
+        final int currentHour = cal.get(Calendar.HOUR_OF_DAY);
+        final int currentMinute = cal.get(Calendar.MINUTE);
+        date.setText(year + "-" + month + "-" + day);
+        time.setText(currentHour + ":" + currentMinute);
+
+
         // on click listener to be able to set the date
         date.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
                 DatePickerDialog dateDialog = new DatePickerDialog(
                         getContext(),
                         dateSetListener,
@@ -85,7 +92,6 @@ public class AddMeasurementDialog extends DialogFragment {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month += 1;
-
                 String dateStr = year + "-" + month + "-" + dayOfMonth;
                 date.setText(dateStr);
             }
@@ -95,9 +101,6 @@ public class AddMeasurementDialog extends DialogFragment {
         time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-                int currentMinute = calendar.get(Calendar.MINUTE);
                 TimePickerDialog timePicker = new TimePickerDialog(getContext(), timeSetListener, currentHour, currentMinute, true);
                 timePicker.show();
             }
@@ -125,7 +128,8 @@ public class AddMeasurementDialog extends DialogFragment {
                         int HRInt = Integer.valueOf(HR.getText().toString());
                         int dBPInt = Integer.valueOf(dBP.getText().toString());
                         int sBPInt = Integer.valueOf(sBP.getText().toString());
-                        if (!comment.getText().toString().isEmpty()) {
+
+                        if (comment.getText().toString().isEmpty()) {
                             listener.onAddPressed(new Measurement(dateStr, timeStr, sBPInt, dBPInt, HRInt));
                         }
                         else {
